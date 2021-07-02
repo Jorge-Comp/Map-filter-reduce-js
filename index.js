bd64ade31bff33f0f7ade6e7b416473e0a8fef92
Jorge-Comp <jlvmurilo3@gmail.com>
2021-07-02 09:48:34 -0300

    Filter e Map

---
+++ index.js
@@ -0,0 +1,35 @@
+const pets =[
+    {
+        name:'rex',
+        type: 'dog',
+        age: 10
+    }, 
+    {
+        name:'miau',
+        type: 'cat',
+        age: 2
+    }, 
+    {
+        name:'peixe',
+        type: 'fish',
+        age: 1
+    }
+]
+
+
+const eMenorQueCinco=(age)=>{
+    return age<5
+}
+const newPets = pets.filter(({age})=>{return eMenorQueCinco(age)})
+//const newPets = pets.filter(({age})=>{return age<5})
+
+
+const petNames = pets.map((pet)=>{
+    return pet.name
+})
+console.log("ALL\n");
+console.log( pets );
+console.log("FILTER\n");
+console.log(newPets);
+console.log("MAP\n");
+console.log(petNames);
