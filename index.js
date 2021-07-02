97812c9646081bde0af16c86951aa8175c4b2058
Jorge-Comp <jlvmurilo3@gmail.com>
2021-07-02 09:50:46 -0300

    Map with For

--- index.js
+++ index.js
@@ -27,9 +27,18 @@ const newPets = pets.filter(({age})=>{return eMenorQueCinco(age)})
 const petNames = pets.map((pet)=>{
     return pet.name
 })
+
+const forEachPetNames = []
+
+pets.forEach((pet)=>{
+    forEachPetNames.push(pet.name)
+})
+
 console.log("ALL\n");
 console.log( pets );
 console.log("FILTER\n");
 console.log(newPets);
 console.log("MAP\n");
 console.log(petNames);
+console.log("MAP WITH FOR\n");
+console.log(forEachPetNames);
