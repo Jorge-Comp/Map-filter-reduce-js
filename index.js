96bc79f33ad493f633b9843ebd1372f0432489ce
Jorge-Comp <jlvmurilo3@gmail.com>
2021-07-02 10:06:26 -0300

    Reduce and comments

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ const eMenorQueCinco=(age)=>{
     return age<5
 }
 const newPets = pets.filter(({age})=>{return eMenorQueCinco(age)})
-//const newPets = pets.filter(({age})=>{return age<5})
+//const newPets = pets.filter(({age})=>{return age<5}) //Filter mais direto
 
 
-const petNames = pets.map((pet)=>{
+const petNames = pets.map((pet)=>{  //map realizado com o nome dos pets(criação de um novo array com os nomes dos pets)
     return pet.name
 })
 
@@ -34,11 +34,17 @@ pets.forEach((pet)=>{
     forEachPetNames.push(pet.name)
 })
 
-console.log("ALL\n");
+const totalAge = pets.reduce((totalAge,pet)=>{ //reduce para realizar um somatória de todas as idades dos pets
+    return totalAge += pet.age
+}, 0)
+
+console.log("\nALL\n");
 console.log( pets );
-console.log("FILTER\n");
+console.log("\nFILTER\n");
 console.log(newPets);
-console.log("MAP\n");
+console.log("\nMAP\n");
 console.log(petNames);
-console.log("MAP WITH FOR\n");
+console.log("\nMAP WITH FOR\n");
 console.log(forEachPetNames);
+console.log("\nREDUCE\n");
+console.log(totalAge);
